4ff438193642c8e3d0a567212daae82b6e77480d
erichon <erichon18@gmail.com>
2021-02-11 00:30:16 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,13 @@ basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         basic.showIcon(IconNames.EigthNote)
         music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
     }
     if (input.buttonIsPressed(Button.B)) {
         basic.showString("T=")
