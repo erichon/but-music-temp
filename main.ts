41345d14d2eb45c2561b3a5ec9c417eb000394ae
erichon <erichon18@gmail.com>
2021-02-11 00:24:49 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
+        basic.showIcon(IconNames.EigthNote)
         music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
     }
     if (input.buttonIsPressed(Button.B)) {
+        basic.showString("T=")
         basic.showNumber(input.temperature())
     }
 })
